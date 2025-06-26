42605d689cbada8bf5be58ed77df6df4c46ddf6a
411448110 <106027009+Ethan590329@users.noreply.github.com>
2025-06-26 12:28:27 +0800

    add

--- 0626/ds_03.java
+++ 0626/ds_03.java
@@ -1,1 +1,26 @@
 
+import java.util.Scanner;
+
+public class ds_03 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("請輸入姓名：");
+        String name = input.nextLine();
+
+        System.out.print("請輸入年齡：");
+        int age = input.nextInt();
+        input.nextLine();
+
+        System.out.print("請輸入城市：");
+        String city = input.nextLine();
+
+        System.out.println("\n=== 個人資訊 ===");
+        System.out.println("姓名：" + name);
+        System.out.println("年齡：" + age);
+        System.out.println("城市：" + city);
+
+        input.close();
+    }
+}
