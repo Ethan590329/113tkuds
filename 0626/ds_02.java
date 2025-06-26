a21785a2aacaf127d859808506389ec5dea5c37d
411448110 <106027009+Ethan590329@users.noreply.github.com>
2025-06-26 12:24:00 +0800

    add

--- 0626/ds_02.java
+++ 0626/ds_02.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class ds_02 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("輸入圓的半徑：");
+        double radius = scanner.nextDouble();
+
+        double area = Math.PI * radius * radius;
+
+        System.out.println("圓的面積為：" + area);
+
+        scanner.close();
+    }
+}
