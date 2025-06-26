3a19217ac50c3e43140227202832134773a1f0db
411448110 <106027009+Ethan590329@users.noreply.github.com>
2025-06-26 12:36:00 +0800

    add

--- 0626/ds_06.java
+++ 0626/ds_06.java
@@ -1,1 +1,25 @@
 
+import java.util.Random;
+
+public class ds_06 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Random rand = new Random();
+
+        System.out.println("隨機產生的陣列元素：");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100); // 0~99
+            System.out.print(numbers[i] + " ");
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("\n陣列中的最大值為：" + max);
+    }
+}
