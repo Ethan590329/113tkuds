e092458d6fc63329a1943998b3ad7c350338aff2
411448110 <106027009+Ethan590329@users.noreply.github.com>
2025-06-26 12:33:56 +0800

    add

--- 0626/ds_05.java
+++ 0626/ds_05.java
@@ -1,1 +1,15 @@
 
+public class ds_05 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 7, 2, 9, 5, 1, 8, 6, 4, 10};
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println("陣列中的元素總和為：" + sum);
+    }
+}
