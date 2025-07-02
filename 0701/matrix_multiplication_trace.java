31d43bd875c0a44e59c7653c0507c81fc9f174fb
Ethan590329 <106027009+Ethan590329@users.noreply.github.com>
2025-07-02 21:05:14 +0800

    Update matrix_multiplication_trace.java

--- 0701/matrix_multiplication_trace.java
+++ 0701/matrix_multiplication_trace.java
@@ -1,1 +1,41 @@
+public class matrix_multiplication_trace {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 2},
+            {3, 4}
+        };
+        int[][] b = {
+            {5, 6},
+            {7, 8}
+        };
+
+        int[][] c = new int[2][2]; 
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("計算位置 c[" + i + "][" + j + "]: ");
+                String formula = "";
+                int sum = 0;
+
+                for (int k = 0; k < 2; k++) {
+                    formula += a[i][k] + "*" + b[k][j];
+                    sum += a[i][k] * b[k][j];
+                    if (k != 1) formula += " + ";
+                }
+
+                formula += " = " + sum;
+                System.out.println(formula);
+                c[i][j] = sum;
+            }
+        }
+
+        System.out.println("\n結果矩陣：");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(c[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
 
