0f870b4d111b80278c1f6debdda058217b536d5a
Ethan590329 <106027009+Ethan590329@users.noreply.github.com>
2025-07-02 21:33:32 +0800

    Update q3binsearch.java

--- 0702/q3binsearch.java
+++ 0702/q3binsearch.java
@@ -1,1 +1,41 @@
+import java.util.Scanner;
+
+public class q3binsearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int key = scanner.nextInt();
+
+        int left = 0;
+        int right = n - 1;
+        int ops = 0;
+        int result = -1;
+
+        while (left <= right) {
+            ops++;
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) {
+                result = mid;
+                break;
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+        System.out.println(ops);
+
+        scanner.close();
+    }
+}
 
