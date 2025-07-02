37e642e0a57eb85fdca9184ed4d69e61021ac4a5
Ethan590329 <106027009+Ethan590329@users.noreply.github.com>
2025-07-02 21:22:00 +0800

    Update q1arraysum.java

--- 0702/q1arraysum.java
+++ 0702/q1arraysum.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class q1arraysum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        int ops = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            sum += num;
+            ops++; 
+        }
+
+        System.out.println(sum);
+        System.out.println(ops);
+
+        scanner.close();
+    }
+}
 
