5d9fa92109d3baf4b15d18301504635eff28245f
Ethan590329 <106027009+Ethan590329@users.noreply.github.com>
2025-07-29 09:46:20 +0800

    Update F07_AnagramPalindrome.java

--- finalexam/F07_AnagramPalindrome.java
+++ finalexam/F07_AnagramPalindrome.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class F07_AnagramPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().toLowerCase();
+        int[] count = new int[26];
+        for (char c : input.toCharArray()) {
+            if (c >= 'a' && c <= 'z') count[c - 'a']++;
+        }
+        int odd = 0;
+        for (int x : count) {
+            if (x % 2 != 0) odd++;
+        }
+        System.out.println(odd <= 1 ? "Possible" : "Impossible");
+    }
+}
 
