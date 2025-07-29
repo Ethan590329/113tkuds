20c8f1fdca36df06891de7a67ce7e8aa94fa2413
Ethan590329 <106027009+Ethan590329@users.noreply.github.com>
2025-07-29 09:54:26 +0800

    Update F10_BSTRangeSum.java

--- finalexam/F10_BSTRangeSum.java
+++ finalexam/F10_BSTRangeSum.java
@@ -1,1 +1,52 @@
+import java.util.*;
+
+public class F10_BSTRangeSum {
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] nodes = sc.nextLine().split(" ");
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+        TreeNode root = buildBST(nodes);
+        int sum = rangeSum(root, L, R);
+        System.out.println(sum);
+    }
+
+    static TreeNode buildBST(String[] nodes) {
+        if (nodes.length == 0 || nodes[0].equals("-1")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        while (!queue.isEmpty() && i < nodes.length) {
+            TreeNode curr = queue.poll();
+            if (!nodes[i].equals("-1")) {
+                curr.left = new TreeNode(Integer.parseInt(nodes[i]));
+                queue.offer(curr.left);
+            }
+            i++;
+            if (i >= nodes.length) break;
+            if (!nodes[i].equals("-1")) {
+                curr.right = new TreeNode(Integer.parseInt(nodes[i]));
+                queue.offer(curr.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    static int rangeSum(TreeNode root, int L, int R) {
+        if (root == null) return 0;
+        if (root.val < L) return rangeSum(root.right, L, R);
+        if (root.val > R) return rangeSum(root.left, L, R);
+        return root.val + rangeSum(root.left, L, R) + rangeSum(root.right, L, R);
+    }
+}
 
